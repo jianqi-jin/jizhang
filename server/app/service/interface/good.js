b037fc5c17fac2217ed03a9429ed53f75d637edd
jin <2359634711@qq.com>
2021-08-08 12:18:35 +0800

    sale num

--- server/app/service/interface/good.js
+++ server/app/service/interface/good.js
@@ -8,7 +8,7 @@ class AccountService extends Service {
       const count = await this.app.mysql.query('select count(*) as total from good where userid = ? and title like ? and status in (?)', [userid, `%${query}%`, statuses]);
       const total = count?.[0]?.total || 0;
       let list = await this.app.mysql.query(['select ',
-      'good.id, good.title, good.description, good.content, good.img, good.total, good.saled_num, good.price, good.status, good.create_date, IFNULL(good.total - SUM(sale_detail.total), 0) as left_num',
+      'good.id, good.title, good.description, good.content, good.img, good.total, good.saled_num, good.price, good.status, good.create_date, IFNULL(good.total - SUM(IFNULL(sale_detail.total, 0)), 0) as left_num',
       ' from good',
       ' left join sale_detail',
       ' on good.id = sale_detail.goods_id',
